14ca52e3a23368efc5e522416345d8c78eecb666
Ranieeery <raniery2003@hotmail.com>
2024-09-12 19:26:44 -0300

    refactor: add JavaScript to generate HTML instead of static site (about section)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,8 @@
 import { createHomeSection } from './build/home.js';
-
+import { createAboutSection } from './build/about.js';
+// import { createSkillsSection } from './build/skills.js';
+// import { createPortfolioSection } from './build/portfolio.js';
+// import { createContactSection } from './build/contact.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     const main = document.createElement('main');
@@ -8,4 +11,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.body.appendChild(main);
 
     main.appendChild(createHomeSection());
+      main.appendChild(createAboutSection());
+    //   main.appendChild(createSkillsSection());
+    //   main.appendChild(createPortfolioSection());
+    //   main.appendChild(createContactSection());
 });
