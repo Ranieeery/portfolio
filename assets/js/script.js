d3a4a55bbf34a99e06512e7cfa58bf5075366296
Ranieeery <raniery2003@hotmail.com>
2024-09-14 10:49:37 -0300

    style: add prettier format

--- assets/js/script.js
+++ assets/js/script.js
@@ -97,28 +97,28 @@ window.addEventListener("scroll", scrollUp);
 
 document.addEventListener("DOMContentLoaded", function () {
   const navMenu = document.getElementById("nav-menu"),
-  navToggle = document.getElementById("nav-toggle"),
-  navClose = document.getElementById("nav-close");
+    navToggle = document.getElementById("nav-toggle"),
+    navClose = document.getElementById("nav-close");
 
-if (navToggle) {
-  navToggle.addEventListener("click", () => {
-    navMenu.classList.add("show-menu");
-  });
-}
+  if (navToggle) {
+    navToggle.addEventListener("click", () => {
+      navMenu.classList.add("show-menu");
+    });
+  }
 
-if (navClose) {
-  navClose.addEventListener("click", () => {
+  if (navClose) {
+    navClose.addEventListener("click", () => {
+      navMenu.classList.remove("show-menu");
+    });
+  }
+
+  const navLink = document.querySelectorAll(".nav__link");
+
+  function linkAction() {
+    const navMenu = document.getElementById("nav-menu");
     navMenu.classList.remove("show-menu");
-  });
-}
-
-const navLink = document.querySelectorAll(".nav__link");
-
-function linkAction() {
-  const navMenu = document.getElementById("nav-menu");
-  navMenu.classList.remove("show-menu");
-}
-navLink.forEach((n) => n.addEventListener("click", linkAction));
+  }
+  navLink.forEach((n) => n.addEventListener("click", linkAction));
 
   const themeButton = document.getElementById("theme-button");
   const darkTheme = "dark-theme";
