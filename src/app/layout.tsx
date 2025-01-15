1276be52b7db3b3afeb3691300f9b1f34e4c38ca
Ranieeery <raniery2003@hotmail.com>
2025-01-14 21:54:09 -0300

    chore: update postcss to version 8.5.1 and enhance postcss configuration with cssnano for improved CSS optimization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import "@/styles/globals.css";
+import "@/styles/globals.min.css";
 
 const poppins = Poppins({
     subsets: ["latin"],
