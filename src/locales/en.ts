a6256737e13bdb3f190383116c987d7b3f2fee98
Ranieeery <raniery2003@hotmail.com>
2025-01-16 09:52:16 -0300

    feat: enhance multilingual support by adding About section translations in English and Portuguese

--- src/locales/en.ts
+++ src/locales/en.ts
@@ -1,17 +1,30 @@
 export const en = {
-  header: {
-    home: "Home",
-    about: "About",
-    skills: "Skills", 
-    portfolio: "Portfolio",
-    contact: "Contact",
-    language: "Language",
-  },
-  home: {
-    title: "Raniery Goulart",
-    subtitle: "Fullstack Developer",
-    social: "Computer Science student with a passion for technology and programming",
-    contact: "Contact me",
-    scroll: "Scroll to see more",
-  },
-}
+    header: {
+        home: "Home",
+        about: "About",
+        skills: "Skills",
+        portfolio: "Portfolio",
+        contact: "Contact",
+        language: "Language",
+    },
+    home: {
+        title: "Raniery Goulart",
+        subtitle: "Fullstack Developer",
+        social: "Computer Science student with a passion for technology and programming",
+        contact: "Contact me",
+        scroll: "Scroll to see more",
+    },
+    about: {
+        title: "About me",
+        subtitle: "Introduction",
+        description: `Desenvolvedor Fullstack com experiência em criação de
+                  APIs REST, integração com páginas front-end, bancos de
+                        dados relacionais e testes unitários, segurança e cloud
+                        com certificados AWS. Estudante de Ciência da Computação
+                        na Uniamérica e formado em Eletroeletrônica no CEFET-MG,
+                        utilizo o conhecimento obtido nas duas áreas para criar
+                        soluções eficientes.
+      `,
+        resume: "Download resume",
+    },
+};
--- src/locales/pt-BR.ts
+++ src/locales/pt-BR.ts
@@ -1,17 +1,17 @@
 export const ptBR = {
-  header: {
-    home: "Início",
-    about: "Sobre",
-    skills: "Habilidades",
-    portfolio: "Portfólio", 
-    contact: "Contato",
-    language: "Idioma",
-  },
-  home: {
-    title: "Raniery Goulart",
-    subtitle: "Desenvolvedor Fullstack",
-    social: "Estudante de Ciência da Computação apaixonado por tecnologia e programação.",
-    contact: "Contato",
-    scroll: "Desça para ver mais",
-  },
+    header: {
+        home: "Início",
+        about: "Sobre",
+        skills: "Habilidades",
+        portfolio: "Portfólio",
+        contact: "Contato",
+        language: "Idioma",
+    },
+    home: {
+        title: "Raniery Goulart",
+        subtitle: "Desenvolvedor Fullstack",
+        social: "Estudante de Ciência da Computação apaixonado por tecnologia e programação.",
+        contact: "Contato",
+        scroll: "Desça para ver mais",
+    },
 };
