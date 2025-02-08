be657038678376b6bb5b1ddb1f0f23051aee49ac
Ranieeery <raniery2003@hotmail.com>
2025-02-08 18:47:55 -0300

    fix: improve Portuguese localization text for clarity and consistency

--- src/locales/pt-BR.ts
+++ src/locales/pt-BR.ts
@@ -17,13 +17,12 @@ export const ptBR = {
     about: {
         title: "Sobre mim",
         subtitle: "Introdução",
-        description: `Desenvolvedor Fullstack com experiência em criação de
+        description: `Desenvolvedor Fullstack com experiência na criação de
                 APIs REST, integração com páginas front-end, bancos de
                 dados relacionais e testes unitários, segurança e cloud
-                com certificados AWS. Estudante de Ciência da Computação
-                na Uniamérica e formado em Eletroeletrônica no CEFET-MG,
-                utilizo o conhecimento obtido nas duas áreas para criar
-                soluções eficientes.`,
+                com certificados AWS. Graduando em Ciência da Computação
+                pela Uniamérica e formado em Eletroeletrônica pelo
+                CEFET-MG.`,
         resume: "Baixar currículo",
         href: "/pdf/Currículo - Raniery Meireles Goulart.pdf",
         info: [
@@ -34,8 +33,8 @@ export const ptBR = {
     },
     skills: {
         title: "Habilidades",
-        subtitle: `Tecnologias com as quais trabalho ou já tive contato no
-                </br> meio acadêmico ou em projetos pessoais.`,
+        subtitle: `Tecnologias com as quais já trabalhei ou tive contato
+      </br>no ambiente acadêmico ou projetos pessoais.`,
         categories: [
             { category: "Linguagens" },
             { category: "Tecnologias & Frameworks" },
@@ -45,11 +44,11 @@ export const ptBR = {
     },
     qualification: {
         title: "Qualificações",
-        subtitle: "Formação e experiência profissional",
+        subtitle: "Educação e experiência profissional",
         tab: [{ title: "Trabalho" }, { title: "Educação" }],
         education: [
             {
-                title: "Ciência da Computação",
+                title: "Bacharel, Ciência da Computação",
                 subtitle: "Uniamérica",
                 dateRange: "jan 2023 - dez 2026",
             },
@@ -59,14 +58,14 @@ export const ptBR = {
                 dateRange: "nov 2022 - abr 2023",
             },
             {
-                title: "Técnico em Eletroeletrônica",
+                title: "Técnico, Eletroeletrônica",
                 subtitle: "CEFET-MG",
                 dateRange: "fev 2018 - mar 2021",
             },
         ],
         work: [
             {
-                title: "Estagiário de Desenvolvimento",
+                title: "Estagiário em Desenvolvimento de Software",
                 subtitle: "Empresa 1",
                 dateRange: "ago 2023 - atual",
             },
@@ -89,15 +88,15 @@ export const ptBR = {
         send: "Projeto",
         projects: {
             medical_clinic: {
-                title: "Clínica</br>Médica API",
+                title: "API de<br />Clínica Médica",
                 image: "/images/projects/medical-clinic-api-with-spring.png",
                 link: "https://github.com/Ranieeery/medical-clinic-api-with-spring",
                 icons: ["uil", "uil-clinic-medical", "services__icon"],
                 tags: ["Java", "MySQL", "JUnit", "Spring Boot", "Swagger"],
                 modal: {
-                    title: "Clínica Médica API",
-                    description: `API Rest para gerenciamento de consultório médico 
-                    desenvolvida com Java, Spring Boot e MySQL.`,
+                    title: "API de Clínica Médica",
+                    description: `API REST para gerenciar uma clínica médica com pacientes,
+                    desenvolvida em Java com Spring Boot e banco de dados MySQL.`,
                     details: [
                         "Java",
                         "Spring Boot 3",
@@ -106,27 +105,27 @@ export const ptBR = {
                         "JUnit",
                         "Mockito",
                         "Swagger",
-                        "Padrões de Projeto",
+                        "Design Patterns",
                     ],
                 },
             },
             ia_chatbot: {
-                title: "I.A para </br>Recomendação",
+                title: "Recomendações</br>com IA",
                 image: "/images/projects/ai.png",
                 link: "https://github.com/Ranieeery/openai-ecommerce-api-integration",
                 icons: ["uil", "uil-meeting-board", "services__icon"],
                 tags: ["Java", "IA", "Spring Boot", "OpenAI"],
                 modal: {
-                    title: "Sistema de recomendação de produtos e-commerce",
-                    description: `Um sistema de recomendação de produtos em Java que usa 
-                    a API da OpenAI para fornecer recomendações personalizadas de produtos 
+                    title: "Sistema de recomendação para e-commerce",
+                    description: `Um sistema de recomendação de produtos em Java que usa a
+                    API da OpenAI para fornecer recomendações personalizadas de produtos
                     e análise de sentimentos para avaliações de clientes.`,
                     details: [
                         "Java",
                         "Maven",
                         "OpenAI API",
                         "JTokkit",
-                        "Inteligência Artificial (I.A.)",
+                        "Inteligência Artificial (IA)",
                     ],
                 },
             },
@@ -138,10 +137,9 @@ export const ptBR = {
                 tags: ["Java", "Spring Boot", "I.A.", "JavaScript"],
                 modal: {
                     title: "ByteBox E-commerce Chatbot",
-                    description: `Uma aplicação Spring Boot que implementa um chatbot 
-                    de atendimento ao cliente para a ByteBox, uma plataforma fictícia 
-                    de comércio eletrônico especializada em componentes de computador 
-                    e eletrônicos.`,
+                    description: `Uma aplicação Spring Boot que implementa um chatbot
+                    de atendimento ao cliente para a ByteBox, uma plataforma fictícia
+                    de e-commerce focada em componentes de computador e eletrônicos.`,
                     details: [
                         "Java",
                         "Spring Boot",
@@ -152,7 +150,7 @@ export const ptBR = {
                         "Marked.js",
                         "OpenAI API",
                         "JTokkit",
-                        "Inteligência Artificial (I.A.)",
+                        "Inteligência Artificial (IA)",
                     ],
                 },
             },
@@ -164,8 +162,8 @@ export const ptBR = {
                 tags: ["C#", ".NET", "MySQL", "ASP.NET Core MVC"],
                 modal: {
                     title: "VendasMVC",
-                    description: `Consiste em uma aplicação Web de um sistema de 
-                    vendas com interface gráfica para manipular o banco de dados.`,
+                    description: `Uma aplicação web para um sistema de vendas com
+                    interface gráfica para manipulação do banco de dados.`,
                     details: [
                         "C#",
                         ".NET",
@@ -176,7 +174,7 @@ export const ptBR = {
                         "HTML",
                         "CSS",
                         "Bootstrap",
-                        "Padrões de Projeto",
+                        "Design Patterns",
                     ],
                 },
             },
@@ -188,9 +186,9 @@ export const ptBR = {
                 tags: ["Ruby", "Jekyll", "Shell", "Docker"],
                 modal: {
                     title: "Blog Pessoal",
-                    description: `Blog pessoal feito com Jekyll em um container
-                    docker onde irei postar sobre tecnologia, programação e 
-                    outros assuntos relacionados que sejam do meu interesse.`,
+                    description: `Blog pessoal feito com Jekyll em um container docker
+                    onde postarei sobre tecnologia, programação e outros assuntos
+                    relacionados que me interessam.`,
                     details: [
                         "Ruby",
                         "Jekyll",
@@ -208,10 +206,10 @@ export const ptBR = {
                 icons: ["uil", "uil-envelope", "services__icon"],
                 tags: ["Java", "MySQL", "JUnit", "Spring Boot", "Swagger"],
                 modal: {
-                    title: "Busca CEP API REST",
-                    description: `Programa que salva uma lista de CEPs extraídas dos
-                    correios em um banco SQL e permite a consulta desses CEPs via API 
-                    REST.`,
+                    title: "API REST de Busca de CEP",
+                    description: `Programa que armazena uma lista de CEPs extraídos
+                    dos Correios em um banco de dados SQL e permite que esses CEPs
+                    sejam consultados via API REST.`,
                     details: [
                         "Java",
                         "Spring Boot",
@@ -231,19 +229,19 @@ export const ptBR = {
                 tags: ["HTML", "CSS", "Firebase"],
                 modal: {
                     title: "Fitlife",
-                    description: `Uma landing page de uma academia fictícia`,
-                    details: ["HTML", "CSS", "Firebase"],
+                    description: `Uma landing page para uma academia fictícia.`,
+                    details: ["HTML", "CSS", "Firebase", "CSS Grid", "Flexbox"],
                 },
             },
             portfolio: {
-                title: "Portfolio",
+                title: "Portfólio",
                 image: "/images/projects/portfolio.png",
                 link: "https://github.com/Ranieeery/raniery.dev",
                 icons: ["uil", "uil-user", "services__icon"],
                 tags: ["TypeScript", "Next.js", "React", "Node.js"],
                 modal: {
-                    title: "Portfolio",
-                    description: `Meu portfólio pessoal feito com Next.js e TypeScript`,
+                    title: "Portfólio",
+                    description: `Meu portfólio pessoal feito com Next.js e TypeScript.`,
                     details: [
                         "TypeScript",
                         "Next.js",
@@ -258,14 +256,14 @@ export const ptBR = {
                 },
             },
             habits: {
-                title: "Registro de</br>Hábitos",
+                title: "Rastreador<br />de Hábitos",
                 image: "/images/projects/habits.png",
                 link: "https://github.com/Ranieeery/Habits-tracker",
                 icons: ["uil", "uil-check-square", "services__icon"],
                 tags: ["TypeScript", "React", "Node.js", "React Native"],
                 modal: {
-                    title: "Registro de Hábitos",
-                    description: `Aplicação para registrar hábitos diários e objetivos, com versão web e mobile`,
+                    title: "Rastreador de Hábitos",
+                    description: `Aplicação para rastrear hábitos e metas, com uma versão web e uma versão mobile.`,
                     details: [
                         "Node.js",
                         "Prisma",
@@ -298,10 +296,10 @@ export const ptBR = {
     },
     contact: {
         title: "Contato",
-        subtitle: "Entre em contato para conversarmos",
-        link: "Entrar em contato",
+        subtitle: "Entre em contato para uma conversa",
+        link: "Enviar um e-mail",
         info: [
-            { title: "Telefone (Celular)", subtitle: "+55 (31) 9 9197-4503" },
+            { title: "Telefone", subtitle: "+55 (31) 9 9197-4503" },
             { title: "E-mail", subtitle: "raniery2003@hotmail.com" },
             { title: "Localização", subtitle: "Contagem, MG" },
         ],
